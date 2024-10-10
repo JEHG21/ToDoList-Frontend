14f87b7845ce0e1384895411af87f0b3de790e05
JEHG21 <johanhg21@gmail.com>
2024-10-10 16:49:10 -0600

    Initial configuration folder components, services, models.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskDetailsComponent } from './components/task-details/task-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: TaskListComponent },
+  { 
+    path: 'add-task', 
+    component: TaskFormComponent },
+  { 
+    path: 'task/:id', 
+    component: TaskDetailsComponent },
+  { 
+    path: 'edit-task/:id', 
+    component: TaskFormComponent }
+];
+
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'https://localhost:44365/api/Tasks';
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  getTask(id: number): Observable<Task> {
+    return this.http.get<Task>(`${this.apiUrl}/${id}`);
+  }
+
+  createTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
+
+  updateTask(id: number, task: Task): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, task);
+  }
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
